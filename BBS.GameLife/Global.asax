ef80ca62b1ac85d1e70197238c2ed87340e40797
鹏远 杨 <2316905030@qq.com>
2019-12-02 14:05:49 +0800

    添加项目文件。

---
+++ BBS.GameLife/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BBS.GameLife.MvcApplication" Language="C#" %>
---
+++ ModelInfo/Comments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelInfo
+{
+    public class Comments
+    {
+        public int CId{ get; set; }
+        public int UId{ get; set; }
+        public string Content{ get; set; }
+        public DateTime CreateDate { get; set; }
+        public int PId{ get; set; }
+        public int FId { get; set; }
+    }
+}
