85adb741a4074355654f6c11a1d62877154c6a70
MECHREVO <MECHREVO@DESKTOP-4E6Q75Q>
2019-12-04 11:22:01 +0800

    22222

--- BBSGame/Controllers/PostsController.cs
+++ BBSGame/Controllers/PostsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using ModelInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,42 @@ namespace BBSGame.Controllers
 {
     public class PostsController : Controller
     {
+        BllOpt bl = new BllOpt();
         // GET: Posts
-        public ActionResult Index()
+        public ActionResult Index(string name="")
+        {
+            return View(bl.ShowPoste(name));
+        }
+        //添加帖子
+        public ActionResult AddPost()
         {
             return View();
         }
+        [HttpPost]
+        public void AddPost(PostsInfo m,out string outname)
+        {
+            int result = bl.AddPoste(m,out outname);
+            if(result>0)
+            {
+                Response.Write("<script>alert('添加帖子成功！');location.href='/Posts/index'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('添加失败！');location.href='/Posts/AddPost'</script>");
+            }
+        }
+        public void DelPost(string IdName, string TableName, int Id)
+        {
+            int result = bl.DelPoste(IdName, TableName, Id);
+            if (result > 0)
+            {
+                Response.Write("<script>alert('删除帖子成功！');location.href='/Posts/index'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('删除失败！');location.href='/Posts/AddPost'</script>");
+            }
+        }
+
     }
 }
